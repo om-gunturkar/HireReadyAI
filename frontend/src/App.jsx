9f620f69792d59b7daa66348a4ecd97df2f84e64
Dixita Jadhav <dixitajadhav10@gmail.com>
2025-11-26 17:48:09 +0530

    Refactor App component to use LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <LandingPage />;
+};
 
-export default App
+export default App;
+
